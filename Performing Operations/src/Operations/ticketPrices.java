f5115470c73cfe9a02bd87c3e9c1e7118ecc6cdf
Tyler Fuqua <tyler@agilityda.com>
2018-01-08 19:17:21 -0700

    Complete

--- Performing Operations/src/Operations/ticketPrices.java
+++ Performing Operations/src/Operations/ticketPrices.java
@@ -5,13 +5,17 @@ public class ticketPrices {
 	public static void main(String[] args) {
 		// an integer to hold our number
 		int age = 15;
+		boolean isStudent = true;
 		
 		// if statement (condition)
-		if (age >= 65) {
+		if (age >= 8) {
 			System.out.println("Senior citizen, pay $7");
+		} if (isStudent = true) {
+			System.out.println("Students, pay $8");
 		} if (age <= 12) {
 			System.out.println("Children, pay $8");
-		} else {
+		}	
+			else {
 			System.out.println("Everyone else, pay $10");
 		}
 
